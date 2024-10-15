572abb87f6b0f2ad9c37deb0729af8ebc838cd91
Muaz Hussain <muaz.self@gmail.com>
2024-10-16 00:05:15 +0600

    Create - config.py

---
+++ infra/config.py
@@ -0,0 +1,7 @@
+class Config:
+    def __init__(self, project_name, instance_type, ami, region, availability_zone) -> None:
+        self.project_name = project_name
+        self.instance_type = instance_type
+        self.ami = ami
+        self.region = region
+        self.availability_zone = availability_zone
